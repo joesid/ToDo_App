2b9c5fc6603e5f8276b096e548a5762e315515d1
joesid <joesidious@gmail.com>
2023-06-28 23:00:59 +0100

    Read and Write Todo Items from File

--- main.py
+++ main.py
@@ -3,22 +3,39 @@ user_prompt = "Enter a todo: "
 todos = []
 
 while True:
-    user_action = input("Type add or show: ")
+    user_action = input("Type add or show or edit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show' | 'display':
-            for item in todos:
-                item = item.title()
-                print(item)
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+            for index, item in enumerate(todos):
+                row = f"{index + 1} - {item}"
+                print(row)
+
         case 'edit':
             number = int(input("Number of the todo to edit "))
-            number -= 1
+            number = number - 1
             new_todo = input("Enter new todo:  ")
             todos[number] = new_todo
+
+        case 'complete':
+            number = int(input("Number of the todo to complete:: "))
+            todos.pop(number)
 
         case 'exit':
             break
