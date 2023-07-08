91bac5e8a03f938b1665f8dc052e2fbc3f0498e2
joesid <joesidious@gmail.com>
2023-07-08 11:33:40 +0100

    Create Function file(separate function)

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-def get_todos(filepath):
-    # read file
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
+from functions import get_todos, write_todos
+import time
 
-
-def write_todos(filepath, todos_arg):
-    """writing file to the todolist"""
-    with open(filepath, 'w') as file_local:
-        file_local.writelines(todos_arg)
-
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("it is now")
 
 user_prompt = "Enter a todo: "
 
@@ -26,7 +19,7 @@ while True:
         todos.append(todo + '\n')
 
         # Opens, Writes and Closes 'todos.txt' file
-        write_todos('todos.txt', todos)
+        write_todos(todos)
 
     elif user_action.startswith('show'):
 
@@ -47,7 +40,7 @@ while True:
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
         except ValueError:
             print("Command is not Valid.")
             continue
@@ -62,7 +55,7 @@ while True:
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
