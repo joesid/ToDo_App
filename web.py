0afee4cf8b2bfd237625a22e363f2509c25d8b3e
joesid <joesidious@gmail.com>
2023-07-11 18:47:04 +0100

    Implement "Complete" feature in WebApp

--- web.py
+++ web.py
@@ -18,9 +18,14 @@ st.write("This app is to increase your productivity")
 # st.checkbox("Throw the trash ")
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
 
-st.session_state
+#st.session_state
